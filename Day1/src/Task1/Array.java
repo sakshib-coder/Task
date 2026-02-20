19d6b6d1018e837ded935d09127947f95bf7a228
sakshib-coder <badukalesakshi429@gmail.com>
2026-02-20 15:58:29 +0530

    Task 1 Completed

---
+++ Day1/src/Task1/Array.java
@@ -0,0 +1,23 @@
+package Task1;
+
+public class Array
+
+{
+    public static void main(String[] args)
+    {
+        int [] number= {10,20,30};
+
+        System.out.println("Array elements are:");
+            for
+            (
+                    int i = 0;
+                    i < number.length; i++) {
+                System.out.println(number[i]);
+            }
+
+        }
+
+
+
+    }
+
